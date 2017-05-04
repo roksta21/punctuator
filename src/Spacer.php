932d364a9741bd7280137dd6249c8174a7ca6ea4
samson <s.mbuthia@silensec.com>
2017-05-04 13:02:51 +0300

    completed the first build

--- src/Spacer.php
+++ src/Spacer.php
@@ -11,19 +11,59 @@ namespace Roksta\Punctuator;
  */
 trait Spacer
 {
+    protected $punctuate_columns;
+
+    /**
+     * Set the columns to punctuate
+     * @return Array
+     */
+    abstract public function setPunctuateColumns() : Array;
+
+    /**
+     * boot the trait
+     */
     protected static function bootSpacer()
     {
         static::creating(function ($model) {
-            isset($model->name) ? $model->name = $model->parseString($model->name) : '';
-            isset($model->description) ? $model->description = $model->parseText($model->description) : '';
+            $model->punctuate();
         });
 
         static::updating(function ($model) {
-            isset($model->name) ? $model->name = $model->parseText($model->name) : '';
-            isset($model->description) ? $model->description = $model->parseText($model->description) : '';
+            $model->punctuate();
         });
     }
 
+    /**
+     * The selects the punctuation depending on the set columns.
+     */
+    protected function punctuate()
+    {
+        $columns = $this->setPunctuateColumns();
+
+        collect($columns)->each(function($column, $type) {
+            collect($column)->each(function($name) use ($type) {
+                if ($type == 'short') {
+                    if (isset($this->$name)) {
+                        $this->$name = $this->parseString($this->name);
+                    }
+                }
+
+                if ($type == 'long') {
+                    if (isset($this->$name)) {
+                        $this->$name = $this->parseText($this->name);
+                    }
+                }
+            });
+        });
+
+        return $this;
+    }
+
+    /**
+     * Parse long texts. Adds spaces and uppercases where necessary.
+     * @param String $text
+     * @return String
+     */
     public function parseText(String $text)
     {
         // Space after . and next word begin with uppercase
@@ -49,6 +89,11 @@ trait Spacer
         return $text;
     }
 
+    /**
+     * Parse short strings and set every word to begin with uppercase
+     * @param String $text
+     * @return string
+     */
     public function parseString(String $text)
     {
         return ucwords($text);
